1c75feb6a2bfca98a8ba9b6bb258146b9464f3d6
tomswanderingvlogs-ux <tomswanderingvlogs@gmail.com>
2025-11-09 04:06:31 -0500

    ini

---
+++ prenup.py
@@ -0,0 +1,13 @@
+j_net = -666
+t_net = -200000
+gme = 21
+
+while True:
+    j_net -= 666
+    t_net += 777666
+    gme += 777
+    print(f"""
+        $GME Ticker Rice:       ${gme}  
+        Joanna Lee Net Worth:   ${j_net}
+        Tom V Net Worth:        ${t_net}
+        """)
